efbb6b1a8fa2d4e8fe2d6038645ad0191442d990
Stephanie Sun (WT) <stephanie@worldreader.org>
2015-07-16 15:42:02 -0700

    Chapter 4?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # return clean title
+  def clean_title(page_title = '')
+    base_title = "Steph's Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
