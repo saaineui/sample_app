a129a2e55f8d2f73abf39e93ae122fe02f8fbc94
saaineui <saaineui@gmail.com>
2016-01-18 15:49:08 -0800

    Spineless

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
   
   # return clean title
   def clean_title(page_title = '')
-    base_title = "Author of The Orientations and Connected"
+    base_title = "Spineless"
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      base_title + " | " + page_title
     end
   end
 end
