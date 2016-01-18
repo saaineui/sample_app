42a082946e6411b8b76fa23c540ca6e07c097b98
saaineui <saaineui@gmail.com>
2016-01-18 12:52:32 -0800

    Chapter 4, style

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
+
   def home
+      @links = ['help','about']
   end
 
   def help
+      @links = ['home','about']
   end
 
   def about
+      @links = ['home','help']
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # return clean title
   def clean_title(page_title = '')
-    base_title = "Steph's Sample App"
+    base_title = "Author of The Orientations and Connected"
     if page_title.empty?
       base_title
     else
