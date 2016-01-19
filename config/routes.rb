4f5baf96b0403b9953cdb42c3bd31af991278cd9
saaineui <saaineui@gmail.com>
2016-01-18 22:11:22 -0800

    Added Users/New controller and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
    root 'static_pages#home'
 
 get 'help' => 'static_pages#help'
+get 'signup' => 'users#new'
 get 'login' => 'static_pages#login'
 
   # Example of regular route:
