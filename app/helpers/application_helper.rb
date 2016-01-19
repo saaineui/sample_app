d8d0767858741598c342d104f33283604e11b86c
saaineui <saaineui@gmail.com>
2016-01-18 23:40:52 -0800

    CC license

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # return clean title
   def clean_title(page_title = '')
-    base_title = "Feebee"
+    base_title = "Spineless"
     if page_title.empty?
       base_title
     else
