60cc9535262af0b16b484fb97ec1969239896dbe
saaineui <saaineui@gmail.com>
2016-01-28 21:44:26 -0800

    Successful User signup; Puma

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
   end
   
   def create
-      @user = User.new(user_params)    # Not the final implementation!
+      @user = User.new(user_params)
       if @user.save
-          # Handle a successful save.
+          flash[:success] = "Your account has been created. Nice work!"
+          redirect_to @user
       else
           render 'new'
       end
