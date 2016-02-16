d8252674117ade9c17a8b4f94543918f2a9961a0
saaineui <saaineui@gmail.com>
2016-02-15 20:32:04 -0800

    Reverse scroll direction with keydown

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,9 +1,16 @@
 (function() {
     var playing = false;
+    var direction = "-";
+    jQuery(window).keydown(function(){
+                    direction = "+";
+    });
+    jQuery(window).keyup(function(){
+                    direction = "-";
+    });
     jQuery(window).scroll(function(){
                     if (!playing) {
                           playing = true;
-                          jQuery("#ebook").animate({ top: "-=80vh" }, 1000, function() {
+                          jQuery("#ebook").animate({ top: direction + "=82vh" }, 1000, function() {
                                                    playing = false;
                           });
                     }
