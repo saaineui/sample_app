76705c5d2316d2baeeddb73093c1d0f85b9a23b5
saaineui <saaineui@gmail.com>
2016-02-19 21:39:07 -0800

    Commented out first users admin migration

--- db/migrate/20160208013254_make_admin_false_default.rb
+++ db/migrate/20160208013254_make_admin_false_default.rb
@@ -1,5 +1,5 @@
-class MakeAdminFalseDefault < ActiveRecord::Migration
-    def change
-        change_column_default :users, :admin, from: nil, to: false
-    end
-end
+#class MakeAdminFalseDefault < ActiveRecord::Migration
+#    def change
+#        change_column_default :users, :admin, from: nil, to: false
+#    end
+#end
