d4094783ad8dae5a33f03afc347beeef1eb2be83
saaineui <saaineui@gmail.com>
2016-02-20 22:06:29 -0800

    Automated scroll wrap height

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,15 +1,37 @@
 (function() {
- var direction, anchor;
- anchor = 0;
- 
-    jQuery(function() {
-        jQuery("#book-nav nav").click(function(){
-            if (jQuery(this).hasClass("up")) { direction = "+"; anchor++; } else { direction = "-"; anchor--; }
-                                      if (anchor <= 0) {
-                    jQuery("#ebook").animate({ top: direction + "=82vh" }, 1000);
-                                      } else {
-                                      anchor = 0;
-                                      }
-        });
-    });
+
+	var direction, scroll_interval;
+	var anchor = 0;
+	var margin = 16*4;
+
+	jQuery(function() {
+	
+		if (document.getElementById('scroll-wrap') != null ) {
+		
+		
+			scroll_interval = jQuery(window).height() - jQuery('header').height() - margin;
+			jQuery('#scroll-wrap').height(scroll_interval);
+
+			window.addEventListener("resize", function() {
+				/* Reset when window resizes or screen orientation changes */
+				scroll_interval = jQuery(window).height() - jQuery('header').height() - margin;
+				jQuery('#scroll-wrap').height(scroll_interval);
+			});
+
+			jQuery("#book-nav nav").click(function(){
+			
+				/* Get scroll direction */
+				if (jQuery(this).hasClass("up")) { direction = "+"; anchor++; } else { direction = "-"; anchor--; }
+				
+				if (anchor <= 0) {
+					jQuery("#ebook").animate({ top: direction + "=" + scroll_interval + "px" }, 1000);
+				} else {
+					anchor = 0;
+				}
+				
+			});
+		}
+		
+	});
+		
 }());
