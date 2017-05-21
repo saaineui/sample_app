2e4330eaa52bcd357a94c83ae003ebb5aa68f0c2
saaineui <saaineui@gmail.com>
2017-05-21 16:46:03 -0700

    book reading integration test

--- test/integration/books_read_test.rb
+++ test/integration/books_read_test.rb
@@ -8,27 +8,48 @@ class BooksReadTest < ActionDispatch::IntegrationTest
     end
     
     test "read without logging in" do
-        get book_path(@books.first)
-		assert_template 'books/show'
+        book = @books.first
         
-        (2..@books.first.max_number_of_locations).each do |location|
-            get book_path(@books.first, location)
-            get book_path(@books.first, location, scroll: Random.rand(1..100))
+        get book_path(book)
+        assert_select "a#next-btn[href=?]", open_book_path(book, 1)
+        
+        (1..book.max_number_of_locations-1).each do |location|
+            get open_book_path(book, location)
+            if location == book.max_number_of_locations-1
+                assert_select "a#next-btn", count: 0
+            else
+                assert_select "a#next-btn[href=?]", open_book_path(book, location+1)
+            end
+
+            get open_book_path(book, location, scroll: Random.rand(1..100))
             assert_template 'books/show'
         end
     end
     
     test "log in and read" do
-        get login_path
+        book = @books.last
+        
         post login_path, session: { email: @user.email, password: 'password' }
-        get book_path(@books.last)
+        assert is_logged_in?
 
+        get book_path(book)
         assert_template 'books/show'
 
-        (2..@books.last.max_number_of_locations).each do |location|
-            get book_path(@books.last, location)
-            get book_path(@books.last, location, scroll: Random.rand(1..100))
+        (1..book.max_number_of_locations-1).each do |location|
+            num_bookmarks = Bookmark.all.count
+            scroll = Random.rand(1..100)
+            save_bookmark_href = new_bookmark_path(book_id: book, location: location, scroll: scroll)
+            
+            get open_book_path(book, location, scroll: scroll)
+            assert_select "a#new-bookmark[href=?]", save_bookmark_href
+            
+            get save_bookmark_href
+            assert_redirected_to open_book_path(book, location, scroll: scroll)
+            
+            follow_redirect!
             assert_template 'books/show'
+            assert_not flash.empty?
+            assert_equal num_bookmarks+1, Bookmark.all.count
         end
     end
     
