712b204a2519251f7e236f3e7941dfeafe9b9d83
saaineui <saaineui@gmail.com>
2017-06-19 01:54:31 -0700

    resize images on galley, tweak line height on galley

--- app/assets/javascripts/galley.js
+++ app/assets/javascripts/galley.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
+     
         $( ".page" ).each(function( index ) {
             var copy = $( this ).clone();
             $( this ).after( copy );
         });
+
+		setTimeout(function(){
+		
+			/* Get line height */
+			var line_height = parseInt( jQuery("#ebook p").css("line-height") );
+			
+			/* Resize book images */
+			$("#ebook figure, #ebook h2").each(function() {
+				box_height = parseInt( $(this).height() ); 
+				$(this).height( (box_height - (box_height % line_height) + line_height).toString() + "px" ); 
+			});
+                    
+                }, 300); /* close delay */			
+
 }());
