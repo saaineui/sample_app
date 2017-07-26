8307cb2ff61c42dbd67150998f73856233599308
saaineui <saaineui@gmail.com>
2017-07-25 18:30:08 -0700

    Bookmarked deleted book bug fix and test

---
+++ test/integration/deleted_books_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class DeletedBooksTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin_user = users(:admin)
+    @book = books(:public)
+  end
+
+  test 'handle deleted bookmarked books flexibly' do
+    post login_path, session: { email: @admin_user.email, password: 'password' }
+    get new_bookmark_path(book_id: @book, location: 1, scroll: 50)
+    delete book_path(@book)
+    get user_path(@admin_user)
+    assert_select 'tr th a', "Book ##{@book.id} can not be found."
+  end
+end
