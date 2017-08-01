cd689cb37b839d5311ab2a0b977024a234580d4b
saaineui <saaineui@gmail.com>
2017-07-31 18:31:36 -0700

    Replace chapter processing instance vars

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,9 +5,6 @@ class SectionsController < ApplicationController
   
   def new
     @book.sections.each(&:delete) # Delete old sections, if any
-    @auto_assign_chapter_nums = params[:upload][:auto_assign_chapter_nums].to_i == 1
-    @chapter_num = 0
-
     process_file
     save_book_or_redirect
   end
@@ -17,23 +14,25 @@ class SectionsController < ApplicationController
   def process_file
     raw_text = Nokogiri::HTML(params[:upload][:ebook_file])
     @book.text_length = raw_text.to_s.length
+    chapters = { auto_assign: params[:upload][:auto_assign_chapter_nums].to_i.eql?(1), num_assigned: 0 }
 
     raw_text.xpath('//section').each_with_index do |section, index|
-      add_a_section(section, index)      
+      chapters = add_a_section(section, index, chapters)      
       section.unlink
     end
   end
 
-  def add_a_section(section, index)
+  def add_a_section(section, index, chapters)
     new_section = Section.new(order: index + 1, title: '', text: section.to_s, book: @book)
 
     if new_chapter?(section)
       new_section.title = get_title(section)
-      new_section.chapter = @chapter_num += 1 if @auto_assign_chapter_nums
+      new_section.chapter = chapters[:num_assigned] += 1 if chapters[:auto_assign]
     end
 
     new_section.indexable = !new_section.title.empty?
     new_section.save
+    chapters
   end
   
   def new_chapter?(data)
