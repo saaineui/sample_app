614a7dc13221b2ee1f78bbe2017b58586ab60f13
saaineui <saaineui@gmail.com>
2017-08-05 15:41:16 -0700

    Issue #30: flash, form ux fixes

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $("#lightbox").show();
     });
   
-    $("#close").click(function(){
-        $("#lightbox").hide();
+    $(".close").click(function(){ // also used to close alerts
+        $(this).parent().fadeOut(function(){ $(this).hide(); });
     });
 });
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = 'You are now logged in.'
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
