75786d88ecca74a15a25f7d9ff1b8fc66013042b
saaineui <saaineui@gmail.com>
2017-08-06 00:16:38 -0700

    Optimize for speed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  
+  config.static_cache_control = 'public, max-age=2592000'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
