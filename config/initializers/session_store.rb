bc2dbb9b70a9d957a75b227983db845f6bf16070
saaineui <saaineui@gmail.com>
2017-08-28 12:17:43 -0700

    Add secure flag to session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spineless_session'
+Rails.application.config.session_store :cookie_store, 
+  key: '_spineless_session',
+  secure: Rails.env.production?, # require encryption in production
+  httponly: true # should be true by default 
