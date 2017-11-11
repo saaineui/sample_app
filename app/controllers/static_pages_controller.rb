f5bd4febae449a6651b7f7bec1a31e462d480f7d
saaineui <saaineui@gmail.com>
2017-11-10 21:19:14 -0800

    Home and Help pages full-width design

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,11 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login
   skip_before_action :require_admin
   
-  def home; end
+  def home
+    render layout: 'full'
+  end
+  
+  def help
+    render layout: 'full'
+  end
 end
