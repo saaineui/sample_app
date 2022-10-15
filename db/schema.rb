f67ecef323ca9b529d1bcd6eed725a472d1e568e
saaineui <saaineui@gmail.com>
2022-10-15 14:51:50 -0700

    Upgrade assistant changes

--- db/schema.rb
+++ db/schema.rb
@@ -2,22 +2,21 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_214442) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_210660) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "book_id"
     t.integer "location"
     t.decimal "scroll", default: "0.0"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.index ["book_id"], name: "index_bookmarks_on_book_id"
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
@@ -25,8 +24,8 @@ ActiveRecord::Schema.define(version: 2020_03_21_214442) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "author"
     t.string "logo_url"
     t.text "epigraph"
@@ -46,8 +45,8 @@ ActiveRecord::Schema.define(version: 2020_03_21_214442) do
     t.string "title"
     t.integer "order"
     t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.boolean "indexable", default: false
     t.integer "chapter"
     t.integer "book_id"
@@ -59,8 +58,8 @@ ActiveRecord::Schema.define(version: 2020_03_21_214442) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
