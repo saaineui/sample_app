a8b22051a09fec463da246b0b15786a99af8759a
saaineui <saaineui@gmail.com>
2022-10-30 12:52:12 -0700

    updates: - Add serif font - increase line height of quotes - fix chapter dropdown height and scroll - speed up page scroll - pale teal should darken to rgb(220, 240, 240)

--- app/assets/javascripts/book_scroll.js
+++ app/assets/javascripts/book_scroll.js
@@ -1,38 +1,38 @@
- /* 
-  * BookScroll 
+ /*
+  * BookScroll
   * ============
   * module for cleanly reflowing and scrolling a book chapter
-  * bundled in application.js 
+  * bundled in application.js
   */
         var BookScroll = (function() {
-          
+
             var FALLBACK_LINE_HEIGHT = 30;
             var FALLBACK_STICKY_BAR_HEIGHT = 41;
             var WRAPPER_PADDING = 16; // #wrapper div padding
             var SCROLL_WRAP_MARGIN = 16; // #scroll-wrap div margin
             var NEXT_BACK_MARGIN = 30; // footer height to clear
             var data;
-          
+
             function round(number, precision) {
               var factor = Math.pow(10, precision);
               var tempNumber = Math.round(number * factor);
               return tempNumber / factor;
             };
-          
+
             function initialize_data(d) {
                 data = Object.assign({}, d);
             }
-          
+
             // grab line_height and scroll_interval from dom - call within load
             function initialize_height_props() {
                 data.line_height = get_line_height(); // 1
-              
+
                 var sticky_bar_height = get_sticky_bar_height(); // 2
                 var all_vertical_padding = sticky_bar_height + WRAPPER_PADDING + SCROLL_WRAP_MARGIN + NEXT_BACK_MARGIN; // 3
                 var box_height = $(window).height() - all_vertical_padding; // 4
                 data.scroll_interval = box_height - (box_height % data.line_height); // 5
             }
-          
+
             function update(prop, value) {
                 data[prop] = value;
             }
@@ -42,7 +42,7 @@
                 $("#progress").css("width", progress + "%");
                 $("#progress-percent").text( Math.floor(progress) + "%" );
             }
-          
+
             function update_bookmark_links() {
                 var scroll_param = "scroll=" + safe_scroll(data.anchor, data.max_clicks);
 
@@ -54,13 +54,13 @@
                     $("#new-bookmark").attr("href", new_bookmark);
                 }
             }
-          
+
             function update_anchor_and_max_clicks() {
-                data.max_clicks = compute_max_clicks(); 
-                data.anchor = compute_anchor(); 
+                data.max_clicks = compute_max_clicks();
+                data.anchor = compute_anchor();
             }
-          
-            // get line height from paragraph or h2 tag; fallback to hard-coded value 
+
+            // get line height from paragraph or h2 tag; fallback to hard-coded value
             function get_line_height() {
                 if ($('#ebook h2').length > 0) {
                     return parseInt( $('#ebook h2').css('line-height') ) / 2;
@@ -70,43 +70,43 @@
                 }
                 return FALLBACK_LINE_HEIGHT;
             }
-              
-            // get sticky_bar_height from sticky bar nav tag; fallback to hard-coded value 
+
+            // get sticky_bar_height from sticky bar nav tag; fallback to hard-coded value
             function get_sticky_bar_height() {
                 if ($('#sticky-bar > nav').length > 0) {
                     return parseInt( $("#sticky-bar > nav").height() );
                 }
                 return FALLBACK_STICKY_BAR_HEIGHT;
             }
-              
+
             // compute accrued progress points for current section
             function compute_section_progress() {
                 return data.anchor * data.section_progress_points / (data.max_clicks + 1);
             }
-          
+
             // scroll string with underscore for decimal point
             function safe_scroll(anchor, max_clicks) {
                 return compute_scroll(anchor, max_clicks).toString().replace('.', '_');
             }
-          
+
             // compute scroll value (integer) for an anchor and max_clicks pair
             function compute_scroll(anchor, max_clicks) {
                 return round(anchor * 100.0 / (max_clicks + 1), 2);
             }
-          
+
             // compute anchor (zero index for which page we are on) from scroll
             function compute_anchor() {
                 if (!is_multipage()) { return 0; } // short pages have no scroll
-              
+
                 normalize_scroll();
-            
+
 		var est_anchor = estimated_anchor();
                 var scroll_diff = compute_scroll(est_anchor, data.max_clicks) - data.scroll;
 
                 if (scroll_diff === 0) { return est_anchor; } // exact match found
-                
+
                 var scroll_diff_next_anchor = compute_scroll(est_anchor + 1, data.max_clicks) - data.scroll;
-              
+
                 if (est_anchor == data.max_clicks || Math.abs(scroll_diff) < Math.abs(scroll_diff_next_anchor)) {
                     return est_anchor;
                 } else {
@@ -115,24 +115,24 @@
             }
 
 	    function estimated_anchor() {
-	        return Math.floor((data.content_height * data.scroll * 0.01) / data.scroll_interval);    
+	        return Math.floor((data.content_height * data.scroll * 0.01) / data.scroll_interval);
 	    }
 
 	    function normalize_scroll() {
-		if (data.scroll < 0) { 
-		    update('scroll', 0);  
-		} else if (estimated_anchor() > data.max_clicks) { 
-		    update('scroll', compute_scroll(data.max_clicks, data.max_clicks)); 
+		if (data.scroll < 0) {
+		    update('scroll', 0);
+		} else if (estimated_anchor() > data.max_clicks) {
+		    update('scroll', compute_scroll(data.max_clicks, data.max_clicks));
 		}
             }
-          
+
             // compute upper bound for scroll
             function compute_max_clicks(){
                 if (!is_multipage()) { return 0; } // short pages have no scroll
-                                      
+
                 if (data.content_height % data.scroll_interval == 0) { // special handling for full height last page
                     return data.content_height / data.scroll_interval - 1;
-                } else { 
+                } else {
                     return Math.floor(data.content_height / data.scroll_interval);
                 }
             }
@@ -148,7 +148,7 @@
             function is_bottom(anchor_increment) {
                 return anchor_increment > 0 && data.anchor === data.max_clicks;
             }
-          
+
             function is_multipage() {
                 return data.content_height > data.scroll_interval;
             }
@@ -158,7 +158,7 @@
                 data: function() {
                     return data;
                 },
-              
+
                 // is a bookmarked link or other prescrolled page
                 is_prescrolled_page: function() {
                     return data.anchor > 0;
@@ -168,7 +168,7 @@
                 get_anchor_increment: function(nav_btn){
                     return $(nav_btn).hasClass("up") ? -1 : 1;
                 },
-              
+
                 // scroll one page up or down and update progress bar and bookmark links
                 scroll_page: function(anchor_increment){
                     if (is_top_or_bottom(anchor_increment)) {
@@ -176,17 +176,17 @@
                     }
 
                     update('anchor', data.anchor + anchor_increment);
-                    $("#ebook").animate({ marginTop: -1 * (data.scroll_interval * data.anchor) + "px" }, 500);
+                    $("#ebook").animate({ marginTop: -1 * (data.scroll_interval * data.anchor) + "px" }, 150);
 
                     update_progress_bar();
                     update_bookmark_links();
-                }, 
-              
+                },
+
                 // prescroll page to computed anchor
                 prescroll_page: function(){
                     $("#ebook").css("margin-top", -1 * (data.scroll_interval * data.anchor) + "px");
                 },
-              
+
                 // expose some private methods
                 compute_anchor: compute_anchor,
                 compute_max_clicks: compute_max_clicks,
@@ -201,5 +201,5 @@
                 update_bookmark_links: update_bookmark_links,
                 update_progress_bar: update_progress_bar
             };
-          
+
         })();
